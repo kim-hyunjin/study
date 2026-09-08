bf92957957c00a76c77b827aa048b577f401ca92
Kim HyunJin <hyunjin1612@gmail.com>
2026-09-08 20:35:04 +0900

    docs(pdf-app): RAG 구현 가이드 추가 (#40)

--- ai/langchain/udemy-masterclass/pdf-app/app/chat/vector_stores/__init__.py
+++ ai/langchain/udemy-masterclass/pdf-app/app/chat/vector_stores/__init__.py
@@ -4,5 +4,5 @@ from functools import partial
 retriever_map = {
     "pinecone_1": partial(build_retriever, k=1),
     "pinecone_2": partial(build_retriever, k=2),
-    "pinecone_3": partial(build_retriever, k=2),
+    "pinecone_3": partial(build_retriever, k=3),
 }
